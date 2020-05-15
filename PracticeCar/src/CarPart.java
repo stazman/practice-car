a34094a47dbf52311499d09d77cad06fb191f460
Christopher DiStasio <cadistasio@gmail.com>
2020-05-15 12:06:50 -0500

    Stubs CarPart class

--- PracticeCar/src/CarPart.java
+++ PracticeCar/src/CarPart.java
@@ -1,4 +1,12 @@
 
-public class CarPart {
+public class CarPart implements Functional {
 
+	int condition;
+	
+	public void status() {
+		
+		System.out.println("The status of this part is " + condition + ".");
+		
+	}
+	
 }
