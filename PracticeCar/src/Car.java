50fa697372c65b95b116b134c600e1b5500a5048
Christopher DiStasio <cadistasio@gmail.com>
2020-05-15 15:31:15 -0500

    Stubs constructor, HashSet initialization, Iterator in Car class

--- PracticeCar/src/Car.java
+++ PracticeCar/src/Car.java
@@ -1,6 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.Iterator;
+
 
 public class Car {
 	
-	public void run(){};
+	Car(Transmission t, Engine e){
+		
+		this.t = t;
+		this.e = e;
+		
+	}
+	
+	Transmission t = new Transmission();
+	Engine e = new Engine();
+	
+	//make static???
+	
+	public void run() {
+		 
+		Set partSet = new HashSet();
+		
+		partSet.add(t);
+		partSet.add(e);
+		
+		Iterator<String> itr = partSet.iterator(); // traversing over HashSet System.out.println("Traversing over Set using Iterator"); while (itr.hasNext()) { System.out.println(itr.next()); }
+
+		while (((Iterator<String>) itr).hasNext()) {
+			
+			t.function();
+			e.function();
+			
+	    }
+		 
+	 }
 
 }
