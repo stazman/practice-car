1a80cb52c1f913b44f4483f69082d3f4b742543d
Christopher DiStasio <cadistasio@gmail.com>
2020-05-15 22:40:33 -0500

    Initializes numberOfSpeeds; edits messages; stubs status()

--- PracticeCar/src/Transmission.java
+++ PracticeCar/src/Transmission.java
@@ -1,20 +1,34 @@
 
 public class Transmission extends CarPart {
-
-	byte numberOfSpeeds;
+	
+	
+	byte numberOfSpeeds = 7;
+	
 	
 	public void shiftGears() {
 		
-		System.out.println("is shifting gears madly on a curvy road!");
+		System.out.print("its gears are being shifted madly on a curvy road!");
 		
 	}
 	
+	public void status(){
+		
+		super.status();
+		
+		int transCond = 100;
+		
+		System.out.println("This car has a condition of " + transCond + ".");
+		
+	}
+	
+	
 	public void function(){
 		
-		System.out.println("This car is a " + numberOfSpeeds + "-speed " + " and ");
-		
+		System.out.print("This car is a " + numberOfSpeeds + "-speed " + "and ");
 		
 		shiftGears();
+		
+		status();
 		
 	}
 	
