25dbb20ff76e0f4c70b5ccb1c7789e055fe0b627
Christopher DiStasio <cadistasio@gmail.com>
2020-05-16 17:30:54 -0500

    Decrements, prints condition of each part correctly on single execution

--- PracticeCar/src/CarPart.java
+++ PracticeCar/src/CarPart.java
@@ -3,24 +3,20 @@ public class CarPart implements Functional {
 
 	public void status() {
 		
-		
 		int condition = 100;
 		
-		
-		if (condition != 0 && condition - 1 <= 1) {
+		if (condition > 0) {
 			
-			int wearAndTear = condition - 1;
+			condition--; 
 			
-			System.out.println("The condition of this part is " + wearAndTear + ".");
+			System.out.println("The condition of this part is " + condition + ".");
 			
-		}
-			 
-		else {
+		} else {
 			
-			System.out.println("Unfortunately, this car is just a really big paperweight now!");
+			System.out.println("The condition of this part is " + condition + ".");
 			
 		}
-		
+			
 	}
 	
 }
