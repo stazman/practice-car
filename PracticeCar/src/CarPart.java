74b0fc86e497b446db8df2ac784fd2f7b3e81b8e
Christopher DiStasio <cadistasio@gmail.com>
2020-05-15 22:44:30 -0500

    Stubs status() in CarPart superclass

--- PracticeCar/src/CarPart.java
+++ PracticeCar/src/CarPart.java
@@ -1,11 +1,25 @@
 
 public class CarPart implements Functional {
 
-	int condition;
-	
 	public void status() {
 		
-		System.out.println("The condition of this part is " + condition + ".");
+		
+		int condition = 100;
+		
+		
+		if (condition != 0 && condition - 1 <= 1) {
+			
+			int wearAndTear = condition - 1;
+			
+			System.out.println("The condition of this part is " + wearAndTear + ".");
+			
+		}
+			 
+		else {
+			
+			System.out.println("Unfortunately, this car is just a really big paperweight now!");
+			
+		}
 		
 	}
 	
